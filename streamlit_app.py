ba5c22a3d5c5c0f23315745059a3fe377c488d55
formationsnowflack <135834642+formationsnowflack@users.noreply.github.com>
2023-06-08 09:59:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ streamlit.text(fruityvice_response)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized
+streamlit.dataframe(fruityvice_normalized)
